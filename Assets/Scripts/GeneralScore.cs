af7d4c63d96e200d7fb5b8116ff638c47a8416aa
FLYNNREAP3R <111404122+FLYNNREAP3R@users.noreply.github.com>
2024-06-26 06:30:00 -0400

    REAP3R/Timer
    - Added timer to the game which can be adjusted from the Unity Editor.
    - Currently displays in 00:00 format

--- Assets/Scripts/GeneralScore.cs
+++ Assets/Scripts/GeneralScore.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 
 public class GeneralScore : MonoBehaviour
 {
+    [SerializeField] private float timeLeft;
 
+    public TextMeshProUGUI timerText;
     public TextMeshProUGUI boxScoreTxt;
     
     // Start is called before the first frame update
     void Start()
     {
+        timerText.text = "";
         boxScoreTxt.text = "Score: ";
     }
 
@@ -19,6 +22,27 @@ public class GeneralScore : MonoBehaviour
     void Update()
     {
         boxScoreTxt.text = "Score: " + GameManager.Instance.GetScore();
+
+        if (timeLeft > 0) 
+        {
+            Debug.Log("Keep going!");
+            timeLeft -= Time.deltaTime;
+            UpdateTImer(timeLeft);
+        }
+        else
+        {
+            Debug.Log("Out of time!");
+            timeLeft = 0;
+        }
     }
 
+    void UpdateTImer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 }
